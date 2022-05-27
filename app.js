81896a5ad2cc169082cff6101e007195e7b38aae
Kavyakrishnan <kavyakrishnan9821@gmail.com>
2022-05-27 09:54:57 +0530

    compltet

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 const Express=require("express")
 const Mongoose=require("mongoose")
 const Bodyparser=require("body-parser")
+const res=require("express/lib/response")
+const req=require("express/lib/response")
 
 
 var app=Express()
-app.get("/bookadd",(req,res)=>{
-    var getTitle=req.body.title
-    var getAuthor=req.body.author
-    var getPrice=req.body.price
-    data={"title":getTitle,"author":getAuthor,"price":getPrice}
+app.use(Bodyparser.urlencoded({extended:true}))
+app.use(Bodyparser.json())
+var busmodel=Mongoose.model("buses",new Mongoose.Schema(
+    {
+Route:String,
+Busname:String,
+Busregno:String,
+Ownername:String,
+Contactno:String
+    }
+   
+
+
+))
+Mongoose.connect("mongodb+srv://appu:2345@cluster0.2q4qp.mongodb.net/busdata")
+app.post("/busapp",(req,res)=>{
+    var getRoute=req.body.Route
+var getBusname=req.body.Busname
+var getBusregno=req.body.Busregno
+var getOwnername=req.body.Ownername
+var getContactno=req.body.Contactno
+
+data={"Route":getRoute,"Busname":getBusname,"Busregno":getBusregno,"Ownername":getOwnername,"Contactno":getContactno}
+    let mybus=new busmodel(data)
+    mybus.save((error,data)=>{
+        if(error){
+            res.sendStatus({"status":"error","data":error})
+        }
+        else{
+            res.sendStatus({"status":"success","data":data}) 
+        }
+     } )
+
+
+
+     res.send(data)
+})
+app.get("/view",(req,res)=>{res.send("view")
 })
 
-
-app.listen(4008,()=>{
-    console.log("success")
+app.listen(5014,()=>{
+    console.log("successing")
 })
